64898222463d5d20a72348b640d8462b9d1be8e9
yhung119 <yhung119@illinois.edu>
2016-07-10 23:37:56 +0800

    templating
    beginning phase of designing the website

---
+++ Styles/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+        $(".nav li").removeClass("active");//this will remove the active class from  
+                                           //previously active menu item 
+        $('#home').addClass('active');
+        
+ });
+
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $(id).offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos}, 800);
+});
